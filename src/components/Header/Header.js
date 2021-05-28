acc318c7a72c1d77cc82356ba1d50a4672acc744
Mark Colby <macolby14@gmail.com>
2021-05-27 15:19:29 -1000

    add header breakpoints

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import { BREAKPOINTS, COLORS, WEIGHTS } from '../../constants';
 import Logo from '../Logo';
+import Icon from "../Icon";
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
 
@@ -21,15 +22,26 @@ const Header = () => {
         <Side>
           <Logo />
         </Side>
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
+        <DesktopOnly>
+          <Nav>
+              <NavLink href="/sale">Sale</NavLink>
+              <NavLink href="/new">New&nbsp;Releases</NavLink>
+              <NavLink href="/men">Men</NavLink>
+              <NavLink href="/women">Women</NavLink>
+              <NavLink href="/kids">Kids</NavLink>
+              <NavLink href="/collections">Collections</NavLink>
+          </Nav>
+        </DesktopOnly>
+        <MobileOnly>
+          <Nav>
+            <Icon id="shopping-bag" />
+            <Icon id="search" />
+            <Icon id="menu" />
+          </Nav>
+        </MobileOnly>
+        <DesktopOnly>
+         <Side />
+        </DesktopOnly>
       </MainHeader>
 
       <MobileMenu
@@ -40,22 +52,59 @@ const Header = () => {
   );
 };
 
+
+const DesktopOnly = styled.div`
+  @media ${BREAKPOINTS.tabletAndDown}{
+    display: none;
+  }
+`;
+
+const MobileOnly = styled.div`
+  display: none;
+
+  @media ${BREAKPOINTS.tabletAndDown}{
+    display: revert;
+  }
+`;
+
 const MainHeader = styled.div`
+  --padding-sides: 32px;
   display: flex;
   align-items: baseline;
-  padding: 18px 32px;
+  padding: 18px var(--padding-sides);
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+
+  @media ${BREAKPOINTS.tabletAndDown}{
+    --padding-sides: 16px;
+    justify-content: space-between;
+  }
+
 `;
 
 const Nav = styled.nav`
   display: flex;
-  gap: 48px;
-  margin: 0px 48px;
+  gap: var(--gap-desktop);
+  margin: 0px var(--gap-desktop);
+
+  
+  @media ${BREAKPOINTS.tabletAndDown}{
+    --gap-desktop: 32px;
+    justify-content: space-between;
+  }
+
+  @media ${BREAKPOINTS.mobileAndDown}{
+    --gap-desktop: 24px;
+  }
 `;
 
 const Side = styled.div`
   flex: 1;
+
+  
+  @media ${BREAKPOINTS.tabletAndDown}{
+    
+  }
 `;
 
 const NavLink = styled.a`
